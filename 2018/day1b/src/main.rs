e6f463df6dcf33626e5d8412476293590dbb2326
Rainer Hinterberger <r.hinterberger@vim.at>
2019-02-09 22:49:14 +0100

    Advent Of Code 2018 / Day 1 part 2

---
+++ 2018/day1b/src/main.rs
@@ -0,0 +1,44 @@
+use std::env;
+use std::process;
+use std::fs;
+use std::collections::HashMap;
+
+fn main()
+{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2
+    {
+        println!("You have to provide a Input-File");
+        process::exit(1);       
+    } 
+
+    let filename = &args[1];
+    let filecontents = fs::read_to_string(filename).unwrap();
+
+    println!("Found first duplicate: {}", find_sum(filecontents));
+}
+
+fn find_sum(filecontents: String) -> i32 
+{
+    let mut sum = 0;
+    let mut sums = HashMap::new();
+
+    loop
+    {
+        let numbers = filecontents.lines().into_iter().map(|valstr| valstr.parse::<i32>());
+    
+        for num in numbers
+        {
+            sum += num.unwrap();
+            if sums.contains_key(&sum)    
+            {
+                return sum;         
+            }
+            else
+            {
+                sums.insert(sum, true);
+            }
+        }
+    }
+}
