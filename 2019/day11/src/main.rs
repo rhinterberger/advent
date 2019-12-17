2f4db53bd312ea0ded1ec7dff9128194d539dd20
Rainer Hinterberger <r@hidev.at>
2019-12-17 23:01:15 +0100

    2019 Day 11, refactor

--- 2019/day11/src/main.rs
+++ 2019/day11/src/main.rs
@@ -2,69 +2,62 @@ use std::{fs, thread};
 use std::sync::mpsc::{channel, Sender, Receiver};
 use std::collections::{HashMap};
 use std::ops::Add;
+// Initial_Value part 1 / 2
+const START_COLOR: i32 = 1;
 
 fn main()
 {
     let filecontents = fs::read_to_string("input.txt").unwrap();
 
-    let mut code: Vec<i64> = filecontents.trim().split(",").map(|value| value.parse::<i64>().unwrap()).collect::<Vec<i64>>();
-    code.resize(code.len()+100_000, 0);
+    let mut paintbot: Vec<i64> = filecontents.trim().split(",").map(|value| value.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+    paintbot.resize(paintbot.len() + 100_000, 0);
 
-    let (tocode, fromparent) = channel();
-    let (toparent, fromcode) = channel();
+    let (tobot, fromparent) = channel();
+    let (toparent, frombot) = channel();
 
-    let mut amplifier_code = code.clone();
-    thread::spawn(move || {
-        run(fromparent, toparent, &mut amplifier_code);
-    });
+    thread::spawn(move || { run(fromparent, toparent, &mut paintbot); });
 
-    let mut direction = Point {x:0,y:-1}; // up
+    let mut direction = Point { x: 0, y: -1 }; // up
+    let mut position = Point { x: 0, y: 0 };
     let mut panels = HashMap::new();
-    let mut position = Point {x:0,y:0};
-    panels.entry(position).or_insert(1);
+    panels.entry(position).or_insert(START_COLOR);
+
     loop {
-        tocode.send(*panels.get(&position).unwrap());
+        tobot.send(*panels.get(&position).unwrap());
 
-        let color:i32 = match fromcode.recv() { Ok(col) => col, _ => break};
-        let change_direction:i32 = match fromcode.recv() { Ok(dir) => dir, _ => break };
+        let color: i32 = match frombot.recv() { Ok(col) => col, _ => break };
 
-        match change_direction {
-            0 => match direction { // Turn Left
-                Point {x:0,y:1}  =>  direction = Point {x:-1, y:0},
-                Point {x:1,y:0}  => direction = Point {x:0, y:1},
-                Point {x:0,y:-1} => direction = Point {x:1, y:0},
-                Point {x:-1,y:0} => direction = Point {x:0, y:-1},
-                _ => {}
-            }
-            1 => match direction { // Turn Right
-                Point { x: 0, y: 1 } => direction = Point { x: 1, y: 0 },
-                Point { x: 1, y: 0 } => direction = Point { x: 0, y: -1 },
-                Point { x: 0, y: -1 } => direction = Point { x: -1, y: 0 },
-                Point { x: -1, y: 0 } => direction = Point { x: 0, y: 1 },
+        match frombot.recv() {
+            Ok(dir) =>  match dir {
+                0 => direction.left(),
+                1 => direction.right(),
                 _ => {}
             },
-            _ => {}
-
-        }
+            _ => break
+        };
 
         panels.insert(position, color);
         position = position.add(direction);
         panels.entry(position).or_insert(0);
     }
 
-    let mut image: Vec<u8> = Vec::new();
-    image.resize(252,' ' as u8);
+    println!("{}", panels.len());
+    if START_COLOR == 1 {
+        let mut image: Vec<u8> = Vec::new();
+        image.resize(252, ' ' as u8);
 
-    for (panel, color) in panels {
-        image[(panel.x.abs()+panel.y*42) as usize]= match color {1=> '*' as u8, _ => ' ' as u8};
+        for (panel, color) in panels {
+            image[(panel.x.abs() + panel.y * 42) as usize] = match color {
+                1 => '*' as u8,
+                _ => ' ' as u8
+            };
+        }
+
+        let imagestr = String::from_utf8(image.clone()).unwrap();
+        for image_line in (0..imagestr.len()).step_by(42) {
+            println!("{:?}", &imagestr[image_line..image_line + 42]);
+        }
     }
-
-    let imagestr = String::from_utf8(image.clone()).unwrap();
-    for image_line in (0..imagestr.len()).step_by(42) {
-        println!("{:?}", &imagestr[image_line..image_line+42]);
-    }
-
-
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
@@ -82,12 +75,32 @@ impl Add for Point {
         }
     }
 }
+impl Point {
+    fn left( &mut self) {
+        match self {
+            Point { x:  0, y:  1 } => { self.x = -1; self.y =  0 },
+            Point { x:  1, y:  0 } => { self.x =  0; self.y =  1 },
+            Point { x:  0, y: -1 } => { self.x =  1; self.y =  0 },
+            Point { x: -1, y:  0 } => { self.x =  0; self.y = -1 },
+            _ => {}
+        }
+    }
+    fn right( &mut self) {
+        match self {
+            Point { x:  0, y:  1 } => { self.x =  1; self.y =  0 },
+            Point { x:  1, y:  0 } => { self.x =  0; self.y = -1 },
+            Point { x:  0, y: -1 } => { self.x = -1; self.y =  0 },
+            Point { x: -1, y:  0 } => { self.x =  0; self.y =  1 },
+            _ => {}
+        }
+    }
+}
 
 pub fn run(input: Receiver<i32>, output: Sender<i32>, code: &mut Vec<i64>)
 {
     let mut pc = 0;
     let mut relbase=0;
-    let mut retval = 0;
+    let mut retval;
 
     loop {
         match code[pc] % 100 {
@@ -149,7 +162,6 @@ pub fn run(input: Receiver<i32>, output: Sender<i32>, code: &mut Vec<i64>)
 
 fn prepare_param_idx(numparam: usize, pc: usize, relbase: usize, code: &Vec<i64> ) -> (usize, usize, usize)
 {
-
     let mut pmask = (code[pc] / 100) as usize;
     let mut param = [0,0,0];
     for i in 0..numparam {
