a03fe0f0ba759b8f9759edfdaae67fc2d7443a70
Rainer Hinterberger <rainer@hinterberger.at>
2022-12-01 17:38:33 +0100

    2022 Day 1

---
+++ 2022/day1/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+fn main() {
+    let input_string = fs::read_to_string("input.txt").expect("Failed to open [input.txt]");
+
+    let mut elves = parse_input(input_string);
+
+    elves.sort_unstable();
+    let sum_top_three: i32 = elves
+        .as_slice()
+        .split_at(elves.len()-3)
+        .1
+        .iter()
+        .sum();
+
+    println!("Part 1: {}", elves.last().unwrap());
+    println!("Part 2:{}", sum_top_three);
+}
+
+fn parse_input(input_string: String) -> Vec<i32> {
+    let mut elves: Vec<i32> = Vec::new();
+    let mut calories=0;
+
+    for line in input_string.lines() {
+        match line {
+            "" => {
+                elves.push(calories);
+                calories = 0;
+            }
+            _ => calories += line.parse::<i32>().unwrap()
+        }
+    }
+    elves
+}
