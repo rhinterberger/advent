30bc23606cd94fd0c2668b20fdb7de3f0c07e82a
Rainer Hinterberger <rainer@hinterberger.at>
2022-12-02 21:50:12 +0100

    2022 Day 1 - Remove sort()

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -3,31 +3,36 @@ use std::fs;
 fn main() {
     let input_string = fs::read_to_string("input.txt").expect("Failed to open [input.txt]");
 
-    let mut elves = parse_input(input_string);
+    let top_three_elves = parse_input(input_string);
+    let sum_top_three :i32 = top_three_elves[0] + top_three_elves[1] + top_three_elves[2];
 
-    elves.sort_unstable();
-    println!("Part 1: {}", elves.last().unwrap());
-
-    let sum_top_three :i32 = elves
-        .into_iter()
-        .rev()
-        .take(3)
-        .sum();
+    println!("Part 1: {}", top_three_elves[0]);
     println!("Part 2: {}", sum_top_three);
 }
 
 fn parse_input(input_string: String) -> Vec<i32> {
-    let mut elves: Vec<i32> = Vec::new();
-    let mut calories=0;
+    let mut top_three: Vec<i32> = vec!(0; 3);
+    let mut calories = 0;
 
     for line in input_string.lines() {
         match line {
-            "" => {
-                elves.push(calories);
-                calories = 0;
-            }
+            "" => {  sort_top_three(&mut top_three, calories); calories = 0; }
             _ => calories += line.parse::<i32>().unwrap()
         }
     }
-    elves
+    top_three
+}
+
+fn sort_top_three(top_three: &mut Vec<i32>, calories: i32) {
+    if calories > top_three[2] {
+        top_three[2] = calories;
+    }
+
+    if top_three[2] > top_three[1] {
+        top_three.swap(1, 2)
+    }
+
+    if top_three[1] > top_three[0] {
+        top_three.swap(0, 1)
+    }
 }
